91dcf15d9b1546dc608e3ed9c3310db811031900
Batalkon <108395903+Batalkon@users.noreply.github.com>
2023-10-04 23:22:48 +0300

    all.py

--- all.py
+++ all.py
@@ -5,7 +5,8 @@ my_name = "YourName"
 
 def hmac_sha256(key, message):
     hashed_key = hashlib.sha256(key.encode()).digest()
-    hmac_hash = hmac.new(hashed_key, message.encode(), hashlib.sha256).digest()
+    hmac_hash = hmac.new(hashed_key, message.encode(),
+    hashlib.sha256).digest()
     return hmac_hash.hex()
 
 # Список целевых IP-адресов для сканирования открытых портов
